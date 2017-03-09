2039eafc9bb9c0bc3c8b85e904c05f1b0dfb3851
Robert Maury <robert.maury@puppetlabs.com>
2017-03-09 11:45:09 -0500

    fix base

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,12 +1,12 @@
 class profile::base {
 
-  class simp {'simp': 
+  class { 'simp': 
     stock_sssd => false,
     sssd       => false,
     scenario   => 'simp',
   }
 
-  class simp_options::dns { 'dns':
+  class { 'simp_options::dns':
     servers => ['8.8.8.8'],
   }
 
