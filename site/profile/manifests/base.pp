a0db095606c8022ac535e9bb45f65070aadd0869
Robert Maury <robert.maury@puppetlabs.com>
2017-03-13 09:15:01 -0400

    blah

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,7 +1,19 @@
 class profile::base {
 
-  class { 'simp_options::dns':
+  class { 'simp_options':
+    auditd       => true,
+    clamav       => true,
+    firewall     => true,
+    ipsec        => true,
+    kerberos     => true,
+    logrotate    => true,
+    pam          => true,
+    selinux      => true,
+    syslog       => true,
     trusted_nets => ['127.0.0.1', '::1','172.28.128.0/24','10.0.2.0/24'],
+  }
+
+  class { 'simp_options::dns':
     servers => '10.0.2.3',
   }
 
