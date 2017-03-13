b6c51a3715b5551275c7e096b783945eb88d4788
Robert Maury <robert.maury@puppetlabs.com>
2017-03-13 09:08:01 -0400

    test

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,14 +1,14 @@
 class profile::base {
 
+  class { 'simp_options::dns':
+    trusted_nets => ['127.0.0.1', '::1','172.28.128.0/24',10.0.2.0/24'],
+    servers => '10.0.2.3',
+  }
+
   class { 'simp': 
     stock_sssd => false,
     sssd       => false,
     scenario   => 'simp',
-  }
-
-  class { 'simp_options::dns':
-    trusted_nets => ['127.0.0.1', '::1','172.28.128.0/24',10.0.2.0/24'],
-    servers => '10.0.2.3',
   }
 
 }
