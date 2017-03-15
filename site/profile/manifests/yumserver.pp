156342b65b01c7cf54833f843d8e3d1298ff9010
Robert Maury <robert.maury@puppetlabs.com>
2017-03-15 10:14:07 -0400

    blah

--- site/profile/manifests/yumserver.pp
+++ site/profile/manifests/yumserver.pp
@@ -1,7 +1,7 @@
 class profile::yumserver {
 
   class { 'simp::server::yum':
-    trusted_nets = ['127.0.0.1','::1','172.28.128.0/24']
+    trusted_nets => ['127.0.0.1','::1','172.28.128.0/24']
   }
 
 }
